df6904be0916c163a4f01b7d00855bbcfed264c5
Thomas <Thomas@DESKTOP-DSC90KN>
2023-10-29 18:17:10 -0700

    Add project files.

---
+++ WPF Section G/Elevator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WPF_Section_G
+{
+    internal class Elevator
+    {
+        private Border Border { get; set; }
+        private Grid Grid { get; set; }
+        private Label Name { get; set; }
+        private Label Floor { get; set; }
+
+        public Elevator(Border border, Grid grid, Label name, Label floorLabel) 
+        {
+            Border = border;
+            Grid = grid;
+            Name = name;
+            Floor = floorLabel;
+        }
+    }
+}
